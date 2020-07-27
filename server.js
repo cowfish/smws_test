d816f0afc8005c3f472daaabfc0b7ed9405b8194
Billy Abbott <billy@cowfish.org.uk>
2020-07-27 20:54:35 +0100

    Fixed port

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ require("./app/routes/distillery.routes")(app);
 require("./app/routes/whisky.routes")(app);
 
 // set port, listen for requests
-const port=8080;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
